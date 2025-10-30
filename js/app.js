6c4f21d1491b954776fa0f77f382c480eaa4a87b
Snudde <kallesalomonsson@gmail.com>
2025-10-30 13:59:19 +0100

    Add theme button elements to app.js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const themeMusicButton = document.getElementById("theme-music");
+const themeMoviesButton = document.getElementById("theme-movies");
+const themeGeographyButton = document.getElementById("theme-geography");
+const themeChristmasButton = document.getElementById("theme-christmas");

307145f99bada2534ed35ad25f922043333067b0
moa.sjoberg@chasacademy.se <moa.sjoberg@chasacademy.se>
2025-11-17 16:52:16 +0100

    update5

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ nameInputBtn.addEventListener("click", function (e) {
 ///LADDAR IN DATAN FRÅN JSON-FILEN
 async function fetchQuiz(themeChoice) {
   try {
-    const response = await fetch("./data/quiz.json");
+    const response = await fetch("/quiz.json");
 
     if (!response.ok) {
       throw new Error(`HTTP error status ${response.status}`);
